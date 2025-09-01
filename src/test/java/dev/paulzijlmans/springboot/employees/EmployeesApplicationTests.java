c04bff661fb135682d1f5cec623109d8ecd1818e
Paul Zijlmans <paulzijlmans@hotmail.com>
2025-09-01 09:16:38 +0200

    first commit

---
+++ src/test/java/dev/paulzijlmans/springboot/employees/EmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.paulzijlmans.springboot.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
