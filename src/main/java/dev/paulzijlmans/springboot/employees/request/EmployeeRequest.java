81f0296de97ed42527db4c31bc73969c6298427a
Paul Zijlmans <paulzijlmans@hotmail.com>
2025-09-01 11:51:37 +0200

    Swagger implementation & Data validation

--- src/main/java/dev/paulzijlmans/springboot/employees/request/EmployeeRequest.java
+++ src/main/java/dev/paulzijlmans/springboot/employees/request/EmployeeRequest.java
@@ -1,4 +1,11 @@
 package dev.paulzijlmans.springboot.employees.request;
 
-public record EmployeeRequest(String firstName, String lastName, String email) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record EmployeeRequest(
+        @NotBlank(message = "First name is mandatory") @Size(min = 2, max = 50, message = "First name must be between 2 and 50 characters") String firstName,
+        @NotBlank(message = "Last name is mandatory") @Size(min = 2, max = 50, message = "Last name must be between 2 and 50 characters") String lastName,
+        @NotBlank(message = "Email is mandatory") @Email(message = "Please provide a valid email address") String email) {
 }
