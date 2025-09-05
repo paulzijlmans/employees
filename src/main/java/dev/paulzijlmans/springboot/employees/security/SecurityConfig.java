6f3b201f348605dc1ad140733637bea51b6bd13c
Paul Zijlmans <paulzijlmans@hotmail.com>
2025-09-05 11:08:54 +0200

    Spring Security Request Mappers

--- src/main/java/dev/paulzijlmans/springboot/employees/security/SecurityConfig.java
+++ src/main/java/dev/paulzijlmans/springboot/employees/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package dev.paulzijlmans.springboot.employees.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -13,24 +18,27 @@ public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
-        UserDetails john = User.builder()
-                .username("john")
-                .password(PASSWORD)
-                .roles("EMPLOYEE")
-                .build();
+        UserDetails john = User.builder().username("john").password(PASSWORD).roles("EMPLOYEE").build();
 
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password(PASSWORD)
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
+        UserDetails mary = User.builder().username("mary").password(PASSWORD).roles("EMPLOYEE", "MANAGER").build();
 
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password(PASSWORD)
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
+        UserDetails susan = User.builder().username("susan").password(PASSWORD).roles("EMPLOYEE", "MANAGER", "ADMIN").build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers("/docs/**", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        http.httpBasic(Customizer.withDefaults());
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
 }
